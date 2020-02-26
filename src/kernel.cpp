ed4c447a9350fee81065deb481db86616dfdf3ab
Hser2bio <hser2bio@icloud.com>
2020-02-26 13:39:35 +0100

    minimal stake input after height

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -21,12 +21,17 @@ bool fTestNet = false; //Params().NetworkID() == CBaseChainParams::TESTNET;
 // Set to 3-hour for production network and 20-minute for test network
 unsigned int nModifierInterval;
 int nStakeTargetSpacing = 60;
+extern unsigned int nStakeMinAge;
 unsigned int getIntervalVersion(bool fTestNet)
 {
     if (fTestNet)
         return MODIFIER_INTERVAL_TESTNET;
-    else
-        return MODIFIER_INTERVAL;
+    else {
+		if (chainActive.Height() > LIMIT_POS_FORK_HEIGHT)
+            return nStakeMinAge / 60;
+        else
+			return MODIFIER_INTERVAL;
+	}
 }
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic
